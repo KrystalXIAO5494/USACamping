ab82acc1941b1654e59fee07634b249dc9c10c26
Crystal Xiao <wxiao5@illinois.edu>
2021-05-12 20:30:42 -0500

    add ejs layout

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground= require('./model/campground');
 const methodOverride=require('method-override');
+const ejsMate = require ('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser: true,
@@ -18,7 +19,7 @@ db.once("open",()=>{
 })
 
 
-
+app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
