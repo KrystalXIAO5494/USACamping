66c4f347159101bb0be01d4dd29f5737e05457e5
Crystal Xiao <wxiao5@illinois.edu>
2021-05-23 16:17:44 -0500

    add expressErrornclass

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const Campground= require('./model/campground');
 const methodOverride=require('method-override');
 const ejsMate = require ('ejs-mate');
+const catchAysnc=require('./utils/catchAsync');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser: true,
@@ -44,23 +45,23 @@ app.get('/campgrounds/:id',async(req,res)=>{
 }
 )
 
-app.get('/campgrounds/:id/edit',async(req,res)=>{
+app.get('/campgrounds/:id/edit',catchAysnc (async(req,res)=>{
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit',{campground});
-}
+})
 )
-app.put('/campgrounds/:id',async(req,res)=>{
+app.put('/campgrounds/:id',catchAysnc( async(req,res)=>{
     const{id}=req.params;
     const campground= await Campground.findByIdAndUpdate(id,{...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`);
-}
+})
 )
 
-app.post('/campgrounds',async(req,res)=>{
+app.post('/campgrounds',catchAysnc(async(req,res)=>{
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-}
+})
 )
 
 app.delete('/campgrounds/:id',async(req,res)=>{
@@ -69,7 +70,9 @@ app.delete('/campgrounds/:id',async(req,res)=>{
     res.redirect('/campgrounds');
 })
 
-
+app.use((err,req,res,next)=>{
+    res.send('oh boy,somthing went wrong')
+})
 
 app.listen(3000,()=>{
     console.log('Serving on port 3000');
