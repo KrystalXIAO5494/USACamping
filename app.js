2187f57f0a3e5a98c4be8a232f9a4b6c230ef3b4
Crystal Xiao <wxiao5@illinois.edu>
2021-06-05 21:05:28 -0500

    create review

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const Campground= require('./model/campground');
 const methodOverride=require('method-override');
 const ejsMate = require ('ejs-mate');
 const catchAysnc=require('./utils/catchAsync');
+const ExpressError=require("./utils/ExpressError");
+const joi=require('joi');
+const Review=require('./model/review')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser: true,
@@ -58,6 +61,7 @@ app.put('/campgrounds/:id',catchAysnc( async(req,res)=>{
 )
 
 app.post('/campgrounds',catchAysnc(async(req,res)=>{
+    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -70,10 +74,24 @@ app.delete('/campgrounds/:id',async(req,res)=>{
     res.redirect('/campgrounds');
 })
 
-app.use((err,req,res,next)=>{
-    res.send('oh boy,somthing went wrong')
-})
+app.post('/campgrounds/:id/reviews',catchAysnc(async(req,res)=>{
+    const campground= await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.review.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 
+}))
+
+app.all("*",(req,res,next)=>{
+    next(new ExpressError('page not found',404))
+})
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
 app.listen(3000,()=>{
     console.log('Serving on port 3000');
 })
